565f6ee3a97d89fac8882900b91be6e2892df159
Alexander Owen <owen.alex47@yahoo.com>
2017-05-03 16:00:20 -0700

    BytecodeExamples: renaming directory

--- BytecodeExamples/int8.java
+++
@@ -1,9 +0,0 @@
-/* Java does not have bytecode instructions to operate on 'byte's. 
- * It treats them as ints, and performs a cast.
- * */
-
-class int8 {
-    public static byte add(byte x, byte y) {
-        return (byte)(x + y);
-    }
-}
